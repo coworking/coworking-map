ca8cf4e7f5adb0e6f26d0cfeb970ff4e6275b854
Ingy döt Net <ingy@ingy.net>
2012-02-19 19:49:36 -0800

    ...

--- sample-map/geoxml3.js
+++ sample-map/geoxml3.js
@@ -143,6 +143,7 @@ geoXML3.parser = function (options) {
         processStyles(doc);
       }
       
+return;
       // Parse placemarks
       if (!!doc.reload && !!doc.markers) {
         for (i = 0; i < doc.markers.length; i++) {
@@ -183,30 +184,30 @@ geoXML3.parser = function (options) {
             doc.bounds.extend(new google.maps.LatLng(placemark.point.lat, placemark.point.lng));
           }
 
-//           if (!!parserOptions.createMarker) {
-//             // User-defined marker handler
-//             parserOptions.createMarker(placemark, doc);
-//           } else {
-//             // Check to see if this marker was created on a previous load of this document
-//             var found = false;
-//             if (!!doc) {
-//               doc.markers = doc.markers || [];
-//               if (doc.reload) {
-//                 for (i = 0; i < doc.markers.length; i++) {
-//                   if (doc.markers[i].getPosition().equals(markerOptions.position)) {
-//                     found = doc.markers[i].active = true;
-//                     break;
-//                   }
-//                 }
-//               } 
-//             }
-// 
-//             if (!found) {
-//               // Call the built-in marker creator
-//               marker = createMarker(placemark, doc);
-//               marker.active = true;
-//             }
-//           }
+          if (!!parserOptions.createMarker) {
+            // User-defined marker handler
+            parserOptions.createMarker(placemark, doc);
+          } else {
+            // Check to see if this marker was created on a previous load of this document
+            var found = false;
+            if (!!doc) {
+              doc.markers = doc.markers || [];
+              if (doc.reload) {
+                for (i = 0; i < doc.markers.length; i++) {
+                  if (doc.markers[i].getPosition().equals(markerOptions.position)) {
+                    found = doc.markers[i].active = true;
+                    break;
+                  }
+                }
+              } 
+            }
+
+            if (!found) {
+              // Call the built-in marker creator
+              marker = createMarker(placemark, doc);
+              marker.active = true;
+            }
+          }
         }
       }
       if (!!doc.reload && !!doc.markers) {
