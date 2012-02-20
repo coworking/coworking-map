72a55aee739e1c9765715cf8dc860da8c3cdd56f
Ingy döt Net <ingy@ingy.net>
2012-02-19 21:09:23 -0800

    ...

--- js/coworking.map.js
+++ js/coworking.map.js
@@ -32,6 +32,11 @@ $Map.generate = function() {
       ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
       ['Maroubra Beach', -33.950198, 151.259302, 1]
     ];
+//     var map = this.map = new google.maps.Map($('#map'), {
+//       zoom: 5,
+//       center: new google.maps.LatLng(39.397, -100.644),
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     });
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
