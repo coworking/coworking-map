67a82d96820454529105dd43be44b3bfa93a634f
Ingy döt Net <ingy@ingy.net>
2012-02-19 21:17:55 -0800

    ...

--- js/coworking.map.js
+++ js/coworking.map.js
@@ -25,15 +25,15 @@ $Map.add_metadata = function(data) {
 };
 
 $Map.generate = function() {
-    console.log(this.metadata);
-    var locations = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
-
+//     console.log(this.metadata);
+//     var locations = [
+//       ['Bondi Beach', -33.890542, 151.274856, 4],
+//       ['Coogee Beach', -33.923036, 151.259052, 5],
+//       ['Cronulla Beach', -34.028249, 151.157507, 3],
+//       ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+//       ['Maroubra Beach', -33.950198, 151.259302, 1]
+//     ];
+// 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 5,
       center: new google.maps.LatLng(39.397, -100.644),
@@ -44,20 +44,20 @@ $Map.generate = function() {
 
     var marker, i;
 
-    for (i = 0; i < locations.length; i++) {  
+    var metadata = this.metadata;
+    for (i = 0, l = metadata.length; i < l; i++) {  
       marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        position: new google.maps.LatLng(metadata[i].geo.lat, metadata[i].geo.long),
         map: map
       });
 
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-          infowindow.setContent(locations[i][0]);
+          infowindow.setContent(metadata[i].Name);
           infowindow.open(map, marker);
         }
       })(marker, i));
     }
-     
 };
 
 // Close  wrapper function and call it.
