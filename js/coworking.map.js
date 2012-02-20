475fd1b30cf7107caa7747175fd29c56dbdf9ff7
Ingy döt Net <ingy@ingy.net>
2012-02-19 18:30:36 -0800

    ...

--- js/coworking.map.js
+++ js/coworking.map.js
@@ -26,15 +26,18 @@ $Map.add_metadata = function(data) {
 };
 
 $Map.generate = function() {
-    console.log(this.data);
+    console.log(1);
     var myLatlng = new google.maps.LatLng(39.397, -100.644);
+    console.log(2);
     var myOptions = {
         zoom: 5,
         center: myLatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    console.log(3);
     var map = this.map =
         new google.maps.Map($('.map_canvas'), myOptions);
+    console.log(4);
 
 //     var geoXml = new geoXML3.parser({map: map});
 //     geoXml.parse('../bgmm/chapter_09/state_capitals.kml');
